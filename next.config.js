6df7072b58a6d317a92de86a4ea947f4c42fdbc2
Caleb Fetzer <caleb.fetzer@gmail.com>
2021-05-24 20:12:59 +0800

    Add target serverless

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ module.exports = {
     future: {
         webpack5: true,
     },
+    target: "serverless",
     webpack: (config, { dev, isServer }) => {
         if (!dev && !isServer) {
             Object.assign(config.resolve.alias, {
